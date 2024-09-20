343ee916af47e15be7f28d86b875faa9fb7fca8b
chungvh97 <140689259+chungvh97@users.noreply.github.com>
2024-09-20 09:50:06 +0700

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   ssr: false,
-  baseURL: '/katuu/',
+  baseURL: '/Katuu/',
   build: {
     transpile: ['vuetify'],
   },
